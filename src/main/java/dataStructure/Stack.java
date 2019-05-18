e17bc2011e1dab9c2686d4944f5286823070e480
Admin <Admin@192.168.1.3>
2019-05-19 00:34:20 +0530

    Stack and ContainerWithWater

--- src/main/java/dataStructure/Stack.java
+++ src/main/java/dataStructure/Stack.java
@@ -2,9 +2,57 @@ package dataStructure;
 
 public class Stack {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int length = 10;
 
+	private int[] value = new int[length];
+
+	int index = -1;
+
+	Stack(int length) {
+		this.length = length;
+	}
+
+	Stack() {
+
+	}
+
+	public void insert(int data) {
+
+		if (value.length - 1 == index) {
+			System.out.println("Stack Overflow");
+			return;
+		}
+
+		value[++index] = data;
+	}
+
+	public void remove() {
+
+		if (index < 0) {
+			System.out.println("Stack underflow");
+			return;
+		}
+
+		value[index--] = 0;
+	}
+
+	public void printlist() {
+		for (int i =0; i <= index; i++) {
+			System.out.println(value[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+
+		stack.insert(2);
+		stack.insert(5);
+		stack.insert(7);
+
+		stack.printlist();
+		stack.remove();
+		System.out.println("after Removal of top element");
+		stack.printlist();
 	}
 
 }
--- src/main/java/problemSolving/ContainerWithMostWater.java
+++ src/main/java/problemSolving/ContainerWithMostWater.java
@@ -3,8 +3,24 @@ package problemSolving;
 public class ContainerWithMostWater {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = { 1, 5, 4, 3 };
+		System.out.println(findMostWater(a));
 	}
 
+	private static int findMostWater(int[] a) {
+		int maxSum = 0;
+
+		int left = 0;
+		int right = a.length - 1;
+		while (left < right) {
+			maxSum = Math.max(maxSum, Math.min(a[left], a[right]) * (right - left));
+
+			if (a[right] > a[left]) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		return maxSum;
+	}
 }
