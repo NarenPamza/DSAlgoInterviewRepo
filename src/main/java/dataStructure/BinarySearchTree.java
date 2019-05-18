21b18257ae64c21e071ff30a5e74be87c79b5a50
Admin <Admin@192.168.1.3>
2019-05-19 00:08:23 +0530

    BST & LL

--- src/main/java/dataStructure/BinarySearchTree.java
+++ src/main/java/dataStructure/BinarySearchTree.java
@@ -1,63 +1,155 @@
 package dataStructure;
 
+
 public class BinarySearchTree {
-	
+
 	Node root;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BinarySearchTree searchTree = new BinarySearchTree();
+		searchTree.insert(2);
+		
+		searchTree.insert(4);
+		searchTree.insert(7);
+		searchTree.insert(5);
+		searchTree.insert(9);
+		searchTree.insert(8);
+		searchTree.displayInOrder();
+		
+		searchTree.delete(4);
+		System.out.println("After Deletion");
+		searchTree.displayInOrder();
+		
+		System.out.println("IsvalidTree " + searchTree.isValidBST());
+		
+		System.out.println("isValuePresent " + searchTree.isValuePresent(5));
 
 	}
-	
+
+	public boolean isValuePresent(int data) {
+		return checkValue(root, data);
+	}
+
+	private boolean checkValue(Node root2, int data) {
+
+		if (root2 == null) {
+			return false;
+		}
+
+		if (root2.value == data) {
+			return true;
+		}
+
+		if (data < root2.value) {
+			return checkValue(root2.left, data);
+		}
+
+		return checkValue(root2.right, data);
+	}
+
+	public void displayInOrder() {
+		printInOrder(root);
+	}
+
+	private void printInOrder(Node root2) {
+		if (root2 != null) {
+			printInOrder(root2.left);
+			System.out.println(root2.value);
+			printInOrder(root2.right);
+		}
+	}
+
 	class Node {
 		int value;
-		Node right,left;
-		
-		Node(int value){
+		Node right, left;
+
+		Node(int value) {
 			this.value = value;
 		}
 	}
-	
-	public void insertRec(int value){
-		root = insert(root, value);
+
+	public void insert(int value) {
+		root = insertValue(root, value);
 	}
-	
-	public boolean isValidBST(){
+
+	public boolean isValidBST() {
 		return isValid(root);
 	}
 
 	int prevValue = -1;
+
 	private boolean isValid(Node root2) {
-		
-		if(root2 == null){
+
+		if (root2 == null) {
 			return true;
 		}
-		
-		if(!isValid(root2.left)){
+
+		if (!isValid(root2.left)) {
 			return false;
 		}
-		
-		if(!(prevValue < root2.value)){
+
+		if (!(prevValue < root2.value)) {
 			return false;
 		}
-		
+
 		prevValue = root2.value;
-		
+
 		return isValid(root2.right);
 	}
 
-	private Node insert(Node root2, int value) {
-		
-		if(root2 == null){
+	private Node insertValue(Node root2, int value) {
+
+		if (root2 == null) {
 			root2 = new Node(value);
 			return root2;
 		}
-		
-		if(value < root2.value){
-			root2.left = insert(root2.left, value);
-		} else if(value > root2.value){
-			root2.right = insert(root2.right, value);
+
+		if (value < root2.value) {
+			root2.left = insertValue(root2.left, value);
+		} else if (value > root2.value) {
+			root2.right = insertValue(root2.right, value);
 		}
 		return root2;
+	}
+	
+	public void delete(int value){
+		root = deleteValue(root,value);
+	}
+
+	private Node deleteValue(Node root, int data) {
+		
+		if(root == null){
+			return root;
+		}
+		
+		if(data < root.value){
+			root.left = deleteValue(root.left, data);
+		} else if(data > root.value){
+			root.right = deleteValue(root.right, data);
+		} else {
+			
+			if(root.left == null){
+				return root.right;
+			} else if( root.right == null){
+				return root.left;
+			}
+			
+			root.value = minValue(root.right);
+			
+			root.right = deleteValue(root.right, root.value);
+		}
+		
+		return root;
+	}
+	
+	private int minValue(Node root){
+		int minValue = root.value;
+		
+		while(root.left != null){
+			minValue = root.left.value;
+			root = root.left;
+		}
+		
+		return minValue;
 	}
 }
--- src/main/java/dataStructure/LinkedList.java
+++ src/main/java/dataStructure/LinkedList.java
@@ -1,10 +1,96 @@
 package dataStructure;
 
 public class LinkedList {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	Node root;
+	
+	class Node {
+		int data;
+		
+		Node next;
+		
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
 	}
 
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.insert(5);
+		list.insert(6);
+		list.insert(9);
+		list.insert(7);
+		
+		list.remove(5);
+		list.printValues();
+		System.out.println("After Reverse");
+		list.reverse();
+		list.printValues();
+
+	}
+	
+	public void remove(int data){
+		
+		if(root!= null && root.data == data){
+			root = root.next;
+			return;
+		}
+		
+		Node tmp = root;
+		Node prevValue = root;
+		while(tmp.next != null){
+			prevValue = tmp;
+			tmp = tmp.next;
+			if(tmp.data == data){
+				break;
+			}
+		}
+		prevValue.next = tmp.next;
+	}
+	
+	public void reverse(){
+		if(root != null && root.next == null){
+			return;
+		}
+		
+		Node current = root;
+		Node prev = null,nextValue = null;
+		while(current != null){
+			nextValue = current.next;
+			current.next = prev;
+			prev = current;
+			current = nextValue;
+		}
+		root = prev;
+	}
+	
+	public void insert(int data){
+		
+		if(root == null){
+			root = new Node(data);
+			return;
+		}
+		
+		Node node = new Node(data);
+		Node tmp = root;
+		while(tmp.next != null){
+			tmp = tmp.next;
+		}
+		tmp.next = node;
+	}
+	
+	public void printValues(){
+		
+		if(root.next == null){
+			System.out.println(root.data);
+			return;
+		}
+		
+		Node tmp = root;
+		while(tmp != null){
+			System.out.println(tmp.data);
+			tmp = tmp.next;
+		}
+	}
 }
