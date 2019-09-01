2d57d579f51a8b500f2a48416de0171693dcb96d
Admin <Admin@192.168.1.6>
2019-09-02 00:15:55 +0530

    Combination Sum

---
+++ src/main/java/dp/CombinationSum.java
@@ -0,0 +1,49 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum {
+
+	public static void main(String[] args) {
+
+		int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
+		int target = 8;
+		List<List<Integer>> combinationSum2 = combinationSum2(candidates, target);
+
+		combinationSum2.forEach((k) -> {
+			k.forEach(System.out::print);
+			System.out.println(" ");
+		});
+
+	}
+
+	private static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+		Arrays.sort(candidates);
+		List<List<Integer>> combinationSum2 = new ArrayList<List<Integer>>();
+		List<Integer> path = new ArrayList<Integer>();
+		dfs_sum(candidates, 0, target, path, combinationSum2);
+		return combinationSum2;
+	}
+
+	private static void dfs_sum(int[] candidates, int index, int target, List<Integer> current,
+			List<List<Integer>> res) {
+		if (target == 0) {
+			res.add(new ArrayList<>(current));
+			return;
+		}
+
+		if (target < 0) {
+			return;
+		}
+
+		for (int i = index; i < candidates.length; i++) {
+			if (i == index || candidates[i] != candidates[i - 1]) {
+				current.add(candidates[i]);
+				dfs_sum(candidates, i + 1, target - candidates[i], current, res);
+				current.remove(current.size() - 1);
+			}
+		}
+	}
+}
