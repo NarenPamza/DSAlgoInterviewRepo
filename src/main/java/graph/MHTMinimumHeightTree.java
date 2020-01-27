8decbb1b6b0d4e224bfb8c960afef85e0d6e86d7
Admin <Admin@192.168.1.5>
2020-01-27 10:23:40 +0530

    Second Approach for MHT

--- src/main/java/graph/MHTMinimumHeightTree.java
+++ src/main/java/graph/MHTMinimumHeightTree.java
@@ -2,7 +2,11 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/minimum-height-trees/
@@ -19,6 +23,8 @@ public class MHTMinimumHeightTree {
 
 		int[][] inputs2 = { { 1, 0 }, { 1, 2 }, { 1, 3 } };
 		System.out.println(mht(4, inputs2));
+
+		System.out.println(findMinHeightTreesUsingQuque(4, inputs2));
 
 	}
 
@@ -60,5 +66,37 @@ public class MHTMinimumHeightTree {
 			leavesNode = nAns;
 		}
 		return leavesNode;
+	}
+
+	public static List<Integer> findMinHeightTreesUsingQuque(int n, int[][] edges) {
+
+		if (n <= 1)
+			return Arrays.asList(0);
+		Set<Integer>[] adj = new Set[n];
+		for (int i = 0; i < n; i++)
+			adj[i] = new HashSet<>();
+		for (int[] e : edges) {
+			adj[e[0]].add(e[1]);
+			adj[e[1]].add(e[0]);
+		}
+
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 0; i < n; i++)
+			if (adj[i].size() == 1)
+				q.offer(i);
+
+		for (int i = n; i > 2;) { // Don't use q.size() to decide. Wrong!!!
+			for (int j = q.size(); j > 0; j--, i--) { // must handle level by
+														// level
+				int v1 = q.poll();
+				int v2 = adj[v1].iterator().next(); // leaf can only have 1
+													// parent (tree)
+				adj[v2].remove(v1);
+				if (adj[v2].size() == 1)
+					q.offer(v2);
+			}
+		}
+		return q.size() == 1 ? Arrays.asList(q.poll()) : Arrays.asList(q.poll(), q.poll());
+
 	}
 }
